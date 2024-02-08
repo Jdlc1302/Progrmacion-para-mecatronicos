828210b4cdaa02b4b4655d40a5c6d23b9afb5841
Jdlc1302 <157074368+Jdlc1302@users.noreply.github.com>
2024-02-08 16:18:26 -0400

    Add files via upload
    Ejercicios del capitulo 1

---
+++ Programaciones de cap 1/Ejercicio 1/Ejercicio 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* Programa 1.1
+El siguiente es mi primer programa escrito en lenguaje C. */
+
+int main (void)
+{
+    printf ("Mi primer programa en C\n");
+    return 0;
+}
