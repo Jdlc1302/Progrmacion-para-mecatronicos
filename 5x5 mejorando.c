f11d039392733f684b292934615ddb07b9fc0130
Jdlc1302 <157074368+Jdlc1302@users.noreply.github.com>
2024-03-07 19:54:15 -0400

    Matrix

---
+++ 5x5 mejorando.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#define TAM 5
+#define COL TAM
+#define ROW TAM
+
+int d1=0;
+int d2=0;
+int matriz[ROW][COL]=
+{
+    {4, 6, 5, 2, 3},
+    {5, 1, 9, 8, 5},
+    {4, 5, 6, 2, 3},
+    {8, 6, 8, 3, 6},
+    {9, 3, 4, 1, 7}
+};
+int dar( int matriz[][TAM], int t)
+{
+    int i,  f;
+    for(i = 0; i < t; i++)
+        for(f = 0; f < t; f++)
+            if(f == i)
+            {
+                printf("\nEstas en la fila %d, columna %d, que tiene el valor: %d\t(dp)",i, f, matriz[i][f]);
+                d1+=matriz[i][f];
+            }
+}
+
+int dar2(int matriz [][TAM], int t)
+{
+    int i, f;
+    for(i = 0; i < t; i++)
+        for(f = 0; f < t; f++)
+        {
+            if(i + f == t - 1)
+        {
+            printf("\nEstas en la fila %d, columna %d, que tiene el valor: %d\t(ds)",i, f, matriz[i][f]);
+            d2+=matriz[i][f];
+        }
+        }
+}
+
+int suma(int matriz[][TAM], int t)
+{
+  int i, f;
+  int sum = 0;
+  for(i = 0; i < t; i++)
+        for(f = 0; f < t; f++)
+            if(f == i)
+        {
+            sum = sum + matriz [i][f];
+            printf("\nLa suma de la diagonal principal es: %d", sum);
+        }
+}
+int suma2(int matriz[][TAM], int t)
+{
+    int i, f;
+    int sum = 0;
+    for(i = 0; i < t; i++)
+        for(f = 0; f < t; f++)
+        {
+            if(i + f == t - 1)
+        {
+           sum = sum + matriz[i][f];
+           printf("\nLa suma de la diagonal secundaria es: %d", sum);
+        }
+        }
+}
+
+int main()
+{
+    int sumat = 0;
+    dar (matriz, TAM);
+    printf("\n");
+    dar2(matriz, TAM);
+    printf("\n");
+    suma(matriz, TAM);
+    printf("\n");
+    suma2(matriz, TAM);
+    printf("\n\n d1=%d", d1);
+    printf("\n\n d2=%d", d2);
+
+    return 0;
+}
+
